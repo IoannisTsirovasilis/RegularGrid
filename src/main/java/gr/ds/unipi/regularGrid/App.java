d28983d2e40bbde6f80fc12e40c4cef51d2ee0a8
Ioannis Tsirovasilis <ioannistsirovasilis@gmail.com>
2020-05-26 13:22:37 +0300

    Initial Commit

---
+++ src/main/java/gr/ds/unipi/regularGrid/App.java
@@ -0,0 +1,13 @@
+package gr.ds.unipi.regularGrid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
